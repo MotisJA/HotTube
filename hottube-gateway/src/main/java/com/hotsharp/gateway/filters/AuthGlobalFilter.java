0a60647802ba2173a88b3cd93408d84969590dbe
MotisJA <115715609+MotisJA@users.noreply.github.com>
2024-12-10 18:05:58 +0800

    refactor(global): knife4j配置统一
    网关聚合有问题，不知道什么原因

--- hottube-gateway/src/main/java/com/hotsharp/gateway/filters/AuthGlobalFilter.java
+++ hottube-gateway/src/main/java/com/hotsharp/gateway/filters/AuthGlobalFilter.java
@@ -1,10 +1,10 @@
 package com.hotsharp.gateway.filters;
 
-import com.hotsharp.common.exception.UnauthorizedException;
 import com.hotsharp.common.utils.JwtUtil;
 import com.hotsharp.common.utils.RedisUtil;
 import com.hotsharp.gateway.config.AuthProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -14,24 +14,9 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 
 import java.util.List;
 
