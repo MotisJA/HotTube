141a8ef4ca436967cc8f7e01f5080f545b3e2926
yuqiu2004 <2506852507@qq.com>
2024-12-13 17:58:28 +0800

    feat(hottube-video) : 视频取消转码=>mp4

--- hottube-video/src/main/java/com/hotsharp/video/service/impl/VideoProcessServiceImpl.java
+++ hottube-video/src/main/java/com/hotsharp/video/service/impl/VideoProcessServiceImpl.java
@@ -56,18 +56,19 @@ public class VideoProcessServiceImpl implements VideoProcessService {
         String uploadId = hash + userId;
         String path = System.getProperty("user.dir") + "/" + fileProperty.getTmp() + uploadId;
         File dir = new File(path);
-        // 1. 合并文件分片
-        String videoPath = mergeChunks(dir, uploadId); // 这里进去不出来了？
-        // 2. 转码 转码后的文件存在m3u8文件夹下
-        String destFolder = System.getProperty("user.dir") + "/" + fileProperty.getM3u8() + uploadId;
-        File destDir = new File(destFolder);
-        if (!destDir.exists()) destDir.mkdir();
-        ffmpegUtil.convertToM3U8(videoPath, destFolder);
+        // 1. 合并文件分片 直接将此视频上传到服务器
+        String videoPath = mergeChunks(dir, uploadId);
+//        // 2. 转码 转码后的文件存在m3u8文件夹下
+//        String destFolder = System.getProperty("user.dir") + "/" + fileProperty.getM3u8() + uploadId;
+//        File destDir = new File(destFolder);
+//        if (!destDir.exists()) destDir.mkdir();
+//        ffmpegUtil.convertToM3U8(videoPath, destFolder);
         // 3. 上传文件 更换m3u8中的路径
         String url = null;
         try {
-            url = uploadM3U8ToMinio(uploadId);
-        } catch (InterruptedException e) {
+//            url = uploadM3U8ToMinio(uploadId);
+            url = uploadVideo(videoPath, uploadId);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
         // 删除缓存
@@ -75,12 +76,19 @@ public class VideoProcessServiceImpl implements VideoProcessService {
         redisUtil.removeCache(key);
         // 删除文件
         String dir1 = System.getProperty("user.dir") + "/" + fileProperty.getTmp() + uploadId;
-        String dir2 = System.getProperty("user.dir") + "/" + fileProperty.getM3u8() + uploadId;
+//        String dir2 = System.getProperty("user.dir") + "/" + fileProperty.getM3u8() + uploadId;
         deleteFile(new File(dir1));
-        deleteFile(new File(dir2));
+//        deleteFile(new File(dir2));
         // 4. 更新数据库中视频文件的信息
         if (StringUtils.isNotEmpty(url)) sendBackUrl(vid, uploadId, url);
         else throw new RuntimeException("error convert : " + uploadId + " url is empty ...");
+    }
+
+    private String uploadVideo(String videoPath, String uploadId) {
+        File file = new File(videoPath);
+        if (!file.exists()) return "";
+        minioUtil.upload(file, uploadId);
+        return minioProperty.getBaseUrl() + FileConstant.MINIO_VIDEO_PREFIX + uploadId + "/" + uploadId + ".mp4";
     }
 
     @Override
@@ -98,7 +106,6 @@ public class VideoProcessServiceImpl implements VideoProcessService {
     /**
      * 上传流视频到服务器
      *    注意需要更改路径
-     *    TODO: 删除切片视频
      * @param uploadId
      */
     private String uploadM3U8ToMinio(String uploadId) throws InterruptedException {
